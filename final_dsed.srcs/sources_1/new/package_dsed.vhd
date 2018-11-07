32f1986f22867e8c121223bc6ee93773a9eced8c
jperezu <jmp51.21@gmail.com>
2018-11-07 12:53:34 +0100

    declaración de package y audio interface

---
+++ final_dsed.srcs/sources_1/new/package_dsed.vhd
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.11.2018 12:43:00
+-- Design Name: 
+-- Module Name: package_dsed - 
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+package package_dsed is
+
+    constant sample_size: integer := 8;
+
+end package_dsed;
