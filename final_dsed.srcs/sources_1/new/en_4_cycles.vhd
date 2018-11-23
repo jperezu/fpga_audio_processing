0671b6d14cc34bf83c27b11acab06f5649a053d9
marsanher <marcos.sanchez.hernandez@alumnos.upm.es>
2018-11-23 13:36:24 +0100

    Funcionalidad completa en_4_cycles

--- final_dsed.srcs/sources_1/new/en_4_cycles.vhd
+++ final_dsed.srcs/sources_1/new/en_4_cycles.vhd
@@ -43,25 +43,32 @@ end en_4_cycles;
 architecture Behavioral of en_4_cycles is
 signal r_reg: std_logic_vector(1 downto 0);
 signal r_next: std_logic_vector(1 downto 0);
+signal clk3_s: std_logic;
+signal en4_s: std_logic;
 signal cuenta: std_logic_vector(2 downto 0);
 signal cuenta_next: std_logic_vector(2 downto 0);
 begin
 --Register
-    process(clk_12megas , reset)
+    process(clk_12megas, clk3_s, reset)
         begin
             if (reset = '1') then
                 r_reg <= (0=>'1', others => '0');
                 cuenta <= (others => '0');
              elsif (clk_12megas'event and clk_12megas = '1') then
                 cuenta <= cuenta_next;
-                r_reg <= r_next;
+                r_reg <= r_next;               
              end if; 
     end process;
+    
 --Next state logic
 r_next <= r_reg(0) & r_reg(1);
 cuenta_next <= (others => '0') when cuenta = "011" else
         std_logic_vector(unsigned(cuenta)+ 1);
+clk3_s <= '1' when cuenta >= "010" else '0';
+en4_s <= '1' when clk3_s = '1' and r_next >= "01" and r_reg ="01" else '0';
 --output logic
 en_2_cycles <= r_reg(1);
-clk_3megas <= '1' when cuenta >= "010" else '0';
+clk_3megas <= clk3_s;
+en_4_cycles <= en4_s;
+
 end Behavioral;
